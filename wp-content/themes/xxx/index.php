36ec70bf11e758865a939b677bdc3e7492acd437
Tanee Bui <tanee.b@itcgroup.io>
2023-06-25 20:44:35 +0700

    Add template blogs and category

--- wp-content/themes/xxx/index.php
+++ wp-content/themes/xxx/index.php
@@ -1,15 +1,59 @@
 <?php get_header(); ?>
-<div class="breadcrumbs">
-  <?php
-  if (function_exists('yoast_breadcrumb')) {
-    yoast_breadcrumb('<div id="breadcrumbs">', '</div>');
-  }
-  ?>
+
+<div class="block-content">
+  <div class="breadcrumbs">
+    <?php
+    if (function_exists('yoast_breadcrumb')) {
+      yoast_breadcrumb('<div id="breadcrumbs">', '</div>');
+    }
+    ?>
+  </div>
+  <main id="main" class="site-main">
+    <?php
+    // Get the current category
+    $current_category = get_queried_object();
+    $category_title = single_cat_title('', false); // Get the category title
+
+    echo '<h1 class="page-title">' . $category_title . '</h1>';
+
+    // Query for posts with the current category
+    $args = array(
+      'post_type'      => 'post',
+      'category_name'  => $current_category->slug,
+      'posts_per_page' => -1, // Display all posts
+    );
+    $query = new WP_Query($args);
+
+    if ($query->have_posts()) :
+      while ($query->have_posts()) :
+        $query->the_post();
+    ?>
+
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+          <header class="entry-header">
+            <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+          </header>
+
+          <?php if (has_post_thumbnail()) : ?>
+            <div class="featured-image">
+              <?php the_post_thumbnail('medium'); ?>
+            </div>
+          <?php endif; ?>
+
+          <div class="entry-content">
+            <?php the_excerpt(); ?>
+            <a href="<?php the_permalink(); ?>" class="read-more">Read More</a>
+          </div>
+        </article><?php the_ID(); ?>
+      <?php
+      endwhile;
+      wp_reset_postdata();
+    else :
+      ?>
+      <p><?php _e('No posts found.'); ?></p>
+    <?php endif; ?>
+
+  </main>
 </div>
-
-
-<main>
-  <h1 class="text-black text-3xl">Blog page</h1>
-</main>
 
 <?php get_footer(); ?>
--- wp-content/themes/xxx/single.php
+++ wp-content/themes/xxx/single.php
@@ -1,19 +1,54 @@
 <?php get_header(); ?>
+
 <div class="breadcrumbs">
   <?php
   if (function_exists('yoast_breadcrumb')) {
     yoast_breadcrumb('<div id="breadcrumbs">', '</div>');
   }
   ?>
+  <div id="primary" class="content-area">
+    <main id="main" class="site-main">
+
+      <?php
+      while (have_posts()) :
+        the_post();
+      ?>
+
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+          <header class="entry-header">
+            <h1 class="entry-title"><?php the_title(); ?></h1>
+          </header>
+
+          <?php if (has_post_thumbnail()) : ?>
+            <div class="featured-image">
+              <?php the_post_thumbnail('medium'); ?>
+            </div>
+          <?php endif; ?>
+
+          <div class="entry-content">
+            <?php the_content(); ?>
+          </div>
+
+          <div class="social-sharing">
+            <h3>Share this post:</h3>
+            <ul class="social-icons">
+              <li>
+                <a href="https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink(); ?>" target="_blank" rel="nofollow">
+                  <i class="fab fa-facebook"></i>
+                </a>
+              </li>
+              <li>
+                <a href="https://twitter.com/intent/tweet?url=<?php the_permalink(); ?>&text=<?php the_title(); ?>" target="_blank" rel="nofollow">
+                  <i class="fab fa-twitter"></i>
+                </a>
+              </li>
+            </ul>
+          </div><!-- .social-sharing -->
+        </article><!-- #post-<?php the_ID(); ?> -->
+
+      <?php endwhile; ?>
+
+    </main>
+  </div>
 </div>
-
-<main>
-
-<div class="block-content">
-
-  <?php echo the_content(); ?>
-</div >
-
-</main>
-
 <?php get_footer(); ?>
